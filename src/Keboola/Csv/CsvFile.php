53e2e638c27cc9c238f15408d750c400e44f6010
Jan-Age Laroo <jan-age@minus3.nl>
2013-07-22 17:44:20 +0200

    Added delimter detection

--- src/Keboola/Csv/CsvFile.php
+++ src/Keboola/Csv/CsvFile.php
@@ -16,20 +16,23 @@ class CsvFile extends \SplFileInfo implements \Iterator
 
 	protected $_delimiter;
 	protected $_enclosure;
-	protected $_escapedBy;
 
 	protected $_filePointer;
 	protected $_rowCounter = 0;
 	protected $_currentRow;
 	protected $_lineBreak;
 
-	public function __construct($fileName, $delimiter = self::DEFAULT_DELIMITER, $enclosure = self::DEFAULT_ENCLOSURE, $escapedBy = "")
+	public function __construct($fileName, $delimiter = self::DEFAULT_DELIMITER, $enclosure = self::DEFAULT_ENCLOSURE)
 	{
 		parent::__construct($fileName);
 
-		$this->_escapedBy = $escapedBy;
-		$this->_setDelimiter($delimiter);
-		$this->_setEnclosure($enclosure);
+        $this->_setEnclosure($enclosure);
+		if (!$delimiter) {
+            // Detect
+            $this->getDelimiter();
+        } else {
+            $this->_setDelimiter($delimiter);
+        }
 
 	}
 
@@ -58,8 +61,11 @@ class CsvFile extends \SplFileInfo implements \Iterator
 	}
 
 	public function getDelimiter()
-	{
-		return $this->_delimiter;
+    {
+        if (!$this->_delimiter) {
+            $this->_delimiter = $this->_detectDelimiter();
+        }
+        return $this->_delimiter;
 	}
 
 	public function getEnclosure()
@@ -69,7 +75,7 @@ class CsvFile extends \SplFileInfo implements \Iterator
 
 	public function getEscapedBy()
 	{
-		return $this->_escapedBy;
+		return $this->getEnclosure();
 	}
 
 	/**
@@ -120,7 +126,7 @@ class CsvFile extends \SplFileInfo implements \Iterator
 			$return[] = $this->getEnclosure()
 				. str_replace($this->getEnclosure(), str_repeat($this->getEnclosure(), 2), $column) . $this->getEnclosure();
 		}
-		return implode($this->getDelimiter(), $return) . "\n";
+		return implode($this->liter(), $return) . "\n";
 	}
 
 	public function getLineBreak()
@@ -139,7 +145,7 @@ class CsvFile extends \SplFileInfo implements \Iterator
 	public function validateLineBreak()
 	{
 		$lineBreak = $this->getLineBreak();
-		if (in_array($lineBreak, array("\r\n", "\n"))) {
+		if (in_array($lineBreak, array("\r\n", "\n", "\r"))) {
 			return $lineBreak;
 		}
 
@@ -149,6 +155,7 @@ class CsvFile extends \SplFileInfo implements \Iterator
 
 	protected  function _detectLineBreak()
 	{
+
 		rewind($this->_getFilePointer());
 		$sample = fread($this->_getFilePointer(), 10000);
 		rewind($this->_getFilePointer());
@@ -253,8 +260,7 @@ class CsvFile extends \SplFileInfo implements \Iterator
 
 		// allow empty enclosure hack
 		$enclosure = !$this->getEnclosure() ? chr(0) : $this->getEnclosure();
-		$escapedBy = !$this->_escapedBy ? chr(0) : $this->_escapedBy;
-		return fgetcsv($this->_getFilePointer(), null, $this->getDelimiter(), $enclosure, $escapedBy);
+		return fgetcsv($this->_getFilePointer(), null, $this->getDelimiter(), $enclosure, '"');
 	}
 
 	protected function _getFilePointer($mode = 'r')
@@ -277,5 +283,126 @@ class CsvFile extends \SplFileInfo implements \Iterator
 				Exception::FILE_NOT_EXISTS, NULL, 'fileNotExists');
 		}
 	}
+
+    /**
+     * Attempts to guess the delimiter of a set of data
+     *
+     * @param string The data you would like to get the delimiter of
+     * @access protected
+     * @return mixed If a delimiter can be found it is returned otherwise false is returned
+     * @todo - understand what's going on here (I haven't yet had a chance to really look at it)
+     */
+    public function _detectDelimiter() {
+
+        $linefeed = $this->getLineBreak();
+        $quotechar = $this->getEnclosure();
+
+        rewind($this->_getFilePointer());
+        $data = fread($this->_getFilePointer(), 10000);
+        rewind($this->_getFilePointer());
+
+        $charcount = count_chars($data, 1);
+
+        $filtered = array();
+        foreach ($charcount as $char => $count) {
+            if ($char == ord($quotechar)) {
+                // exclude the quote char
+                continue;
+            }
+            if ($char == ord(" ")) {
+                // exclude spaces
+                continue;
+            }
+            if ($char >= ord("a") && $char <= ord("z")) {
+                // exclude a-z
+                continue;
+            }
+            if ($char >= ord("A") && $char <= ord("Z")) {
+                // exclude A-Z
+                continue;
+            }
+            if ($char >= ord("0") && $char <= ord("9")) {
+                // exclude 0-9
+                continue;
+            }
+            if ($char == ord("\n") || $char == ord("\r")) {
+                // exclude linefeeds
+                continue;
+            }
+            $filtered[$char] = $count;
+        }
+
+        // count every character on every line
+        $data = explode($linefeed, $data);
+        $tmp = array();
+        $linecount = 0;
+        foreach ($data as $row) {
+            if (empty($row)) {
+                continue;
+            }
+
+            // count non-empty lines
+            $linecount++;
+
+            // do a charcount on this line, but only remember the chars that
+            // survived the filtering above
+            $frequency = array_intersect_key(count_chars($row, 1), $filtered);
+
+            // store the charcount along with the previous counts
+            foreach ($frequency as $char => $count) {
+                if (!array_key_exists($char, $tmp)) {
+                    $tmp[$char] = array();
+                }
+                $tmp[$char][] = $count; // this $char appears $count times on this line
+            }
+        }
+
+        // a potential delimiter must be present on every non-empty line
+        foreach ($tmp as $char=>$array) {
+            if (count($array) < 0.98 * $linecount) {
+                // ... so drop any delimiters that aren't
+                unset($tmp[$char]);
+            }
+        }
+
+        foreach ($tmp as $char => $array) {
+            // a delimiter is very likely to occur the same amount of times on every line,
+            // so drop delimiters that have too much variation in their frequency
+            $dev = $this->deviation($array);
+            if ($dev > 0.5) { // threshold not scientifically determined or something
+                unset($tmp[$char]);
+                continue;
+            }
+
+            // calculate average number of appearances
+            $tmp[$char] = array_sum($tmp[$char]) / count($tmp[$char]);
+        }
+
+        // now, prefer the delimiter with the highest average number of appearances
+        if (count($tmp) > 0) {
+            asort($tmp);
+            $delim = chr(end(array_keys($tmp)));
+        } else {
+            // no potential delimiters remain
+            $delim = false;
+        }
+
+        return $delim;
+
+    }
+
+    /**
+     * @todo - understand what's going on here (I haven't yet had a chance to really look at it)
+     */
+    protected function deviation ($array){
+
+        $avg = array_sum($array) / count($array);
+        foreach ($array as $value) {
+            $variance[] = pow($value - $avg, 2);
+        }
+        $deviation = sqrt(array_sum($variance) / count($variance));
+        return $deviation;
+
+    }
 
 }
